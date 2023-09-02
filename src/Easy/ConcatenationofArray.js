6ac151ed4686d6b418627c74d4c7d82429006758
Sanjay186 <sanjay.infortech@gmail.com>
2023-09-02 13:57:51 +0530

    array problems

---
+++ src/Easy/ConcatenationofArray.js
@@ -0,0 +1,21 @@
+/**
+ * concatenationofArray
+ * 
+ * 
+ Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    return [...nums,...nums];
+};
+
+console.log(getConcatenation([1,3,2,1]))
